1a8eaaacea3ae8f9731d484feddfeab479999a87
magicgoal <magicgoal.dev@gmail.com>
2025-05-21 16:29:54 -0400

    Backend Deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,13 +4,26 @@ const connectDB = require("./database");
 const typeDefs = require("./schema/typeDefs");
 const userResolver = require("./resolvers/userResolver");
 const employeeResolver = require("./resolvers/employeeResolver");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 connectDB();
 
-const server = new ApolloServer({ typeDefs, resolvers: [userResolver, employeeResolver] });
+// CORS configuration
+app.use(cors({
+  origin: ['https://emp-hub-f.netlify.app', 'http://localhost:4200'],
+  credentials: true
+}));
+
+const server = new ApolloServer({ 
+  typeDefs, 
+  resolvers: [userResolver, employeeResolver],
+  cors: true
+});
+
 server.start().then(() => {
   server.applyMiddleware({ app });
-  app.listen(4000, () => console.log("Server running at http://localhost:4000/graphql"));
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}/graphql`));
 });
